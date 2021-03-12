73bf96a959085916262e2b2d8b33ff4208cc401a
Thomas Senfter <t.senfter@accessio.at>
2021-03-12 16:42:48 +0100

    better same shape as input with negative output_shape

--- CPU/warp_affine_op.cpp
+++ CPU/warp_affine_op.cpp
@@ -11,25 +11,33 @@ constexpr ngraph::NodeTypeInfo WarpAffineOp::type_info;
 
 WarpAffineOp::WarpAffineOp(const ngraph::Output<ngraph::Node>& image,
                            const ngraph::Output<ngraph::Node>& transformation_matrix,
-                           const ngraph::PartialShape& output_shape)
-        : Op({image, transformation_matrix}), output_shape_(output_shape){
+                           const ngraph::PartialShape& output_shape,
+                           bool keep_scale_on_reshape,
+                           const std::vector<float>& scale)
+        : Op({image, transformation_matrix}), output_shape_(output_shape), keep_scale_on_reshape_(keep_scale_on_reshape), scale_(scale){
     constructor_validate_and_infer_types();
 }
 
 void WarpAffineOp::validate_and_infer_types() {
-    if(output_shape_.all_non_negative())
-        set_output_type(0, get_input_element_type(0), output_shape_);
+    if(keep_scale_on_reshape_){
+        std::vector<ngraph::Dimension> out_shape(get_input_partial_shape(0));
+        out_shape[2] = ngraph::Dimension(out_shape[2].get_min_length() * scale_[0], out_shape[2].get_max_length() * scale_[0]);
+        out_shape[3] = ngraph::Dimension(out_shape[3].get_min_length() * scale_[1], out_shape[3].get_max_length() * scale_[1]);
+        set_output_type(0, get_input_element_type(0), out_shape);
+    }
     else
-        set_output_type(0, get_input_element_type(0), get_input_partial_shape(0));
+        set_output_type(0, get_input_element_type(0), output_shape_);
 }
 
 std::shared_ptr<ngraph::Node> WarpAffineOp::clone_with_new_inputs(const ngraph::OutputVector &new_args) const {
     check_new_args_count(this, new_args);
-    return std::make_shared<WarpAffineOp>(new_args.at(0), new_args.at(1), output_shape_);
+    return std::make_shared<WarpAffineOp>(new_args.at(0), new_args.at(1), output_shape_, keep_scale_on_reshape_, scale_);
 }
 
 bool WarpAffineOp::visit_attributes(ngraph::AttributeVisitor &visitor) {
     visitor.on_attribute("output_shape", output_shape_);
+    visitor.on_attribute("keep_scale_on_reshape", keep_scale_on_reshape_);
+    visitor.on_attribute("scale", scale_);
     return true;
 }
 
--- CPU/warp_affine_op.hpp
+++ CPU/warp_affine_op.hpp
@@ -11,12 +11,16 @@ namespace CustomLayers {
             WarpAffineOp() = default;
             WarpAffineOp(const ngraph::Output<ngraph::Node>& image,
                          const ngraph::Output<ngraph::Node>& transformation_matrix,
-                         const ngraph::PartialShape& pshape);
+                         const ngraph::PartialShape& output_shape,
+                         bool keep_scale_on_reshape,
+                         const std::vector<float>& scale);
             void validate_and_infer_types() override;
             std::shared_ptr<ngraph::Node> clone_with_new_inputs(const ngraph::OutputVector& new_args) const override;
             bool visit_attributes(ngraph::AttributeVisitor& visitor) override;
         protected:
             ngraph::PartialShape output_shape_;
+            bool keep_scale_on_reshape_;
+            std::vector<float> scale_;
     };
 }
 //! [fft_op:header]
